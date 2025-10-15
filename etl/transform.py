c11737caf399093741bbf01a703fcfe2357d5880
nicolibelem <nicolibelemdasilva@gmail.com>
2025-10-15 20:54:13 -0300

    Update transform.py
    Parte 2. Transformação (T)
    Item 3 e 4

--- etl/transform.py
+++ etl/transform.py
@@ -1,1 +1,7 @@
+#Remover ou corrigir preços negativos
+if 'preco_unitario' in df.columns:
+    df = df[df['preco_unitario'] >= 0]
 
+#Criar uma nova coluna valor_total = quantidade * preco_unitario
+if 'quantidade' in df.columns and 'preco_unitario' in df.columns:
+    df['valor_total'] = df['quantidade'] * df['preco_unitario']
